f45f3c4d302e0e7e28c96fe7378133328275a40f
hacksalot <hacksalot@indevious.com>
2019-07-21 17:47:18 -0400

    [feat] remove Google Analytics code

--- src/js/xkcdown-site.js
+++ src/js/xkcdown-site.js
@@ -295,12 +295,6 @@ Site-specific JavaScript file for xkc(down) (https://xkcdown.indevious.com).
 
   // Google Analytics
   /* jshint ignore:start */
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-58879362-1', 'auto');
-  ga('send', 'pageview');
   /* jshint ignore:end */
 
 }($, extrovert));
